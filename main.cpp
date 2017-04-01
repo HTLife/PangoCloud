a7c73fd4bc3a46aea961d19ac7612dbc3dd2eedd
jackyliu <jjkka132@gmail.com>
2017-04-01 16:59:23 +0800

    Matching by SIFT

--- main.cpp
+++ main.cpp
@@ -11,9 +11,16 @@
 #include <iostream>
 #include <pangolin/pangolin.h>
 #include <pangolin/gl/gldraw.h>
-
+#include <pcl/common/transforms.h>
 #include "main.h"
 
+
+PangoCloud *cloud;
+PangoCloud *cloud2;
+CAMERA_INTRINSIC_PARAMETERS camera;
+FRAME frame1, frame2;
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr ptCloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr ptCloud2 (new pcl::PointCloud<pcl::PointXYZRGB>);
 
 struct CustomType
 {
@@ -52,7 +59,7 @@ void init()
     pangolin::ParseVarsFile("app.cfg");
 
     // Create OpenGL window in single line
-    pangolin::CreateWindowAndBind("Main",1024,768);
+    pangolin::CreateWindowAndBind("PangoCloud - ICP",1024,768);
 
     // 3D Mouse handler requires depth testing to be enabled
     glEnable(GL_DEPTH_TEST);
@@ -79,7 +86,9 @@ void init()
     // Specialisations mean no conversions take place for exact types
     // and conversions between scalar types are cheap.
     pangolin::Var<bool> a_button("ui.A_Button",false,false);
-    pangolin::Var<double> a_double("ui.A_Double",3,0,5);
+
+    pangolin::Var<double> a_double("ui.A_Double",4,1.1,20);
+
     pangolin::Var<int> an_int("ui.An_Int",2,0,5);
     pangolin::Var<double> a_double_log("ui.Log_scale var",3,1,1E4, true);
     pangolin::Var<bool> a_checkbox("ui.A_Checkbox",false,true);
@@ -100,6 +109,11 @@ void init()
     // Demonstration of how we can register a keyboard hook to trigger a method
     pangolin::RegisterKeyPressCallback(pangolin::PANGO_CTRL + 'r', SampleMethod);
     // Default hooks for exiting (Esc) and fullscreen (tab).
+
+
+
+    loadImages();
+    double dLastTh = 0;
     while( !pangolin::ShouldQuit() )
     {
         // Clear entire screen
@@ -137,7 +151,11 @@ void init()
         //glTranslatef(0.0f, 0.0f, -100.0f);
         //glColor4f(0,0,1,1);
         //pangolin::glDrawLine(0,0,0, 0,0,1);
-
+        if (dLastTh != a_double)
+        {
+            dLastTh = a_double;
+            calculate(a_double);
+        }
         draw();
         //glBegin(GL_LINES);
         //      glPointSize(20.0);
@@ -156,19 +174,16 @@ void init()
 
 void draw()
 {
+    cloud->drawPoints();
+    cloud2->drawPoints();
+}
+
+void loadImages()
+{
     /// RGB
     cv::Mat rgbMat = imread("../1.png", cv::IMREAD_UNCHANGED);
-    //    IplImage *rgbimg = cvLoadImage("../1.png", CV_LOAD_IMAGE_UNCHANGED);
-    //    if(rgbimg == NULL)
-    //    {
-    //        return;
-    //    }
     cv::Mat rgbMat2 = imread("../2.png", cv::IMREAD_UNCHANGED);
-    //    IplImage *rgbimg2 = cvLoadImage("../2.png", CV_LOAD_IMAGE_UNCHANGED);
-    //    if(rgbimg == NULL)
-    //    {
-    //        return;
-    //    }
+
 
     /// Depth
     cv::Mat depthMat = imread("../1d.png", cv::IMREAD_UNCHANGED);
@@ -180,19 +195,73 @@ void draw()
 
     //int32_t depthSize = depthMat.total() * depthMat.elemSize();
 
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr ptCloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr ptCloud2 (new pcl::PointCloud<pcl::PointXYZRGB>);
+    //pcl::PointCloud<pcl::PointXYZRGB>::Ptr ptCloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+    //pcl::PointCloud<pcl::PointXYZRGB>::Ptr ptCloud2 (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+
 
     convertMatToCloud(ptCloud, depthMat, rgbMat);
     convertMatToCloud(ptCloud2, depthMat2, rgbMat2);
 
 
-    PangoCloud cloud(ptCloud.get());
-    PangoCloud cloud2(ptCloud2.get());
-    cloud.drawPoints();
+
+    frame1.rgb = rgbMat;
+    frame1.depth = depthMat;
+    frame2.rgb = rgbMat2;
+    frame2.depth = depthMat2;
+
+
+    // 提取特征并计算描述子
+    cout<<"extracting features"<<endl;
+    string detecter = "SIFT";
+    string descriptor = "SIFT";
+
+    computeKeyPointsAndDesp( frame1, detecter, descriptor );
+    computeKeyPointsAndDesp( frame2, detecter, descriptor );
+
+
 }
 
+void calculate(double th)
+{
+    RESULT_OF_PNP result = estimateMotion( frame1, frame2, camera, th );
+    cout<<result.rvec<<endl<<result.tvec<<endl;
 
+    // 处理result
+    // 将旋转向量转化为旋转矩阵
+    cv::Mat R;
+    cv::Rodrigues( result.rvec, R );
+    Eigen::Matrix3d r;
+    cv::cv2eigen(R, r);
+
+    // 将平移向量和旋转矩阵转换成变换矩阵
+    Eigen::Isometry3d T = Eigen::Isometry3d::Identity();
+
+    Eigen::AngleAxisd angle(r);
+    cout<<"translation"<<endl;
+    Eigen::Translation<double,3> trans(result.tvec.at<double>(0,0), result.tvec.at<double>(0,1), result.tvec.at<double>(0,2));
+    T = angle;
+    T(0,3) = result.tvec.at<double>(0,0);
+    T(1,3) = result.tvec.at<double>(0,1);
+    T(2,3) = result.tvec.at<double>(0,2);
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr ptCloud_new (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::transformPointCloud( *ptCloud2, *ptCloud_new, T.matrix() );
+    //Eigen::Vector3f v3f(0.0, 0.0, 0.0);
+    //Eigen::Quaternionf rotation (0.966, 0, -0.259, 0);
+
+
+    //pcl::transformPointCloud(*ptCloud2, *ptCloud2, v3f, rotation);
+    //pcl::transformPointCloud (
+    //    const pcl::PointCloud< PointT > &cloud_in,
+    //    pcl::PointCloud< PointT > &cloud_out,
+    //    const Eigen::Matrix< Scalar, 3, 1 > &offset,
+    //    const Eigen::Quaternion< Scalar > &rotation)
+
+    cloud = new PangoCloud(ptCloud.get());
+    cloud2 = new PangoCloud(ptCloud_new.get());
+
+}
 
 void
 convertMatToCloud(
@@ -255,14 +324,149 @@ convertMatToCloud(
     //return ptCloud;
 }
 
+
+
+//void getIncrementalTransformation(
+//        Eigen::Vector3f & trans,
+//        Eigen::Matrix<float, 3, 3, Eigen::RowMajor> & rot,
+//        const DeviceArray2D<unsigned short> & depth,
+//        const DeviceArray2D<PixelRGB> & image,
+//        unsigned char * rgbImage,
+//        unsigned short * depthData)
+//{
+
+//}
+
+
+
+
+
+// computeKeyPointsAndDesp 同时提取关键点与特征描述子
+void computeKeyPointsAndDesp( FRAME& frame, string detector, string descriptor )
+{
+    cv::Ptr<cv::FeatureDetector> _detector;
+    cv::Ptr<cv::DescriptorExtractor> _descriptor;
+
+    cv::initModule_nonfree();
+    _detector = cv::FeatureDetector::create( detector.c_str() );
+    _descriptor = cv::DescriptorExtractor::create( descriptor.c_str() );
+
+    if (!_detector || !_descriptor)
+    {
+        cerr<<"Unknown detector or discriptor type !"<<detector<<","<<descriptor<<endl;
+        return;
+    }
+
+    _detector->detect( frame.rgb, frame.kp );
+    _descriptor->compute( frame.rgb, frame.kp, frame.desp );
+
+    return;
+}
+
+// estimateMotion 计算两个帧之间的运动
+// 输入：帧1和帧2
+// 输出：rvec 和 tvec
+RESULT_OF_PNP estimateMotion(
+        FRAME& frame1,
+        FRAME& frame2,
+        CAMERA_INTRINSIC_PARAMETERS& camera,
+        double th)
+{
+    static ParameterReader pd;
+    vector< cv::DMatch > matches;
+    cv::FlannBasedMatcher matcher;
+    matcher.match( frame1.desp, frame2.desp, matches );
+
+    cout<<"find total "<<matches.size()<<" matches."<<endl;
+    vector< cv::DMatch > goodMatches;
+    double minDis = 9999;
+    //double good_match_threshold = atof( pd.getData( "good_match_threshold" ).c_str() );
+    double good_match_threshold = th;//4
+    for ( size_t i=0; i<matches.size(); i++ )
+    {
+        if ( matches[i].distance < minDis )
+            minDis = matches[i].distance;
+    }
+
+    cout << "minDis=" << minDis << endl;
+    cout << "th=" << good_match_threshold << endl;
+    for ( size_t i=0; i<matches.size(); i++ )
+    {
+
+        cout << matches[i].distance << endl;
+        if (matches[i].distance < good_match_threshold*minDis)
+        {
+            goodMatches.push_back( matches[i] );
+        }
+    }
+
+    cout<<"good matches: "<<goodMatches.size()<<endl;
+    // 第一个帧的三维点
+    vector<cv::Point3f> pts_obj;
+    // 第二个帧的图像点
+    vector< cv::Point2f > pts_img;
+
+    // 相机内参
+    for (size_t i=0; i<goodMatches.size(); i++)
+    {
+        // query 是第一个, train 是第二个
+        cv::Point2f p = frame1.kp[goodMatches[i].queryIdx].pt;
+        // 获取d是要小心！x是向右的，y是向下的，所以y才是行，x是列！
+        ushort d = frame1.depth.ptr<ushort>( int(p.y) )[ int(p.x) ];
+        if (d == 0)
+            continue;
+        pts_img.push_back( cv::Point2f( frame2.kp[goodMatches[i].trainIdx].pt ) );
+
+        // 将(u,v,d)转成(x,y,z)
+        cv::Point3f pt ( p.x, p.y, d );
+        cv::Point3f pd = point2dTo3d( pt, camera );
+        pts_obj.push_back( pd );
+    }
+
+    double camera_matrix_data[3][3] = {
+        {camera.fx, 0, camera.cx},
+        {0, camera.fy, camera.cy},
+        {0, 0, 1}
+    };
+
+    cout<<"solving pnp"<<endl;
+    // 构建相机矩阵
+    cv::Mat cameraMatrix( 3, 3, CV_64F, camera_matrix_data );
+    cv::Mat rvec, tvec, inliers;
+    // 求解pnp
+    cv::solvePnPRansac( pts_obj, pts_img, cameraMatrix, cv::Mat(), rvec, tvec, false, 100, 1.0, 100, inliers );
+
+    RESULT_OF_PNP result;
+    result.rvec = rvec;
+    result.tvec = tvec;
+    result.inliers = inliers.rows;
+
+    return result;
+}
+
+
+
+cv::Point3f point2dTo3d( cv::Point3f& point, CAMERA_INTRINSIC_PARAMETERS& camera )
+{
+    cv::Point3f p; // 3D 点
+    p.z = double( point.z ) / camera.scale;
+    p.x = ( point.x - camera.cx) * p.z / camera.fx;
+    p.y = ( point.y - camera.cy) * p.z / camera.fy;
+    return p;
+}
+
+
+
 int main(int argc, char* argv[])
 {  
 
 
-
+    camera.fx = 525.0;
+    camera.fy = 525.0;
+    camera.cx = 319.5;
+    camera.cy = 239.5;
+    camera.scale = 5000;
     init();
-
-
 
     return 0;
 }
--- main.h
+++ main.h
@@ -3,12 +3,100 @@
 
 #include "PangoCloud.h"
 #include <opencv2/opencv.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/core/eigen.hpp>
+
+#include<iostream>
+using namespace std;
 
 void draw();
+void calculate(double th);
 void convertMatToCloud(
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr &ptCloud,
     cv::Mat &depthMat,
     cv::Mat &rgbMat);
+void loadImages();
 
 
+// 帧结构
+struct FRAME
+{
+    cv::Mat rgb, depth; //该帧对应的彩色图与深度图
+    cv::Mat desp;       //特征描述子
+    vector<cv::KeyPoint> kp; //关键点
+};
+
+// PnP 结果
+struct RESULT_OF_PNP
+{
+    cv::Mat rvec, tvec;
+    int inliers;
+};
+
+// computeKeyPointsAndDesp 同时提取关键点与特征描述子
+void computeKeyPointsAndDesp( FRAME& frame, string detector, string descriptor );
+
+struct CAMERA_INTRINSIC_PARAMETERS
+{
+    double cx, cy, fx, fy, scale;
+};
+
+// estimateMotion 计算两个帧之间的运动
+// 输入：帧1和帧2, 相机内参
+RESULT_OF_PNP estimateMotion(
+        FRAME& frame1,
+        FRAME& frame2,
+        CAMERA_INTRINSIC_PARAMETERS& camera,
+        double th);
+
+
+
+// 参数读取类
+class ParameterReader
+{
+public:
+    ParameterReader( string filename="./parameters.txt" )
+    {
+        ifstream fin( filename.c_str() );
+        if (!fin)
+        {
+            cerr<<"parameter file does not exist."<<endl;
+            return;
+        }
+        while(!fin.eof())
+        {
+            string str;
+            getline( fin, str );
+            if (str[0] == '#')
+            {
+                // 以‘＃’开头的是注释
+                continue;
+            }
+
+            int pos = str.find("=");
+            if (pos == -1)
+                continue;
+            string key = str.substr( 0, pos );
+            string value = str.substr( pos+1, str.length() );
+            data[key] = value;
+
+            if ( !fin.good() )
+                break;
+        }
+    }
+    string getData( string key )
+    {
+        map<string, string>::iterator iter = data.find(key);
+        if (iter == data.end())
+        {
+            cerr<<"Parameter name "<<key<<" not found!"<<endl;
+            return string("NOT_FOUND");
+        }
+        return iter->second;
+    }
+public:
+    map<string, string> data;
+};
+
+cv::Point3f point2dTo3d( cv::Point3f& point, CAMERA_INTRINSIC_PARAMETERS& camera );
 #endif // MAIN_H
